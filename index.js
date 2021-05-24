638f0adec3b27f08b478db6794c18b07db041f7c
trung.git <trunght1810@gmail.com>
2021-05-24 22:47:25 +0700

    Init Author Route

--- index.js
+++ index.js
@@ -3,20 +3,29 @@ const app = express();
 const expressLayouts = require("express-ejs-layouts");
 const mongoose = require("mongoose");
 require("dotenv").config();
-
+const port = process.env.PORT || 3000;
+// Require Router
 const indexRoute = require("./routes/indexRoute");
-
+const authorRoute = require("./routes/authorRoute");
+// Set up Views, Layouts
 app.use(express.static("./public"));
 app.set("view engine", "ejs");
 app.set("views", __dirname + "/views/");
 app.set("layout", "layouts/layout");
 app.use(expressLayouts);
 
-app.use("/", indexRoute);
+//Set up body parser
+app.use(express.json());
+app.use(express.urlencoded());
 
-const port = process.env.PORT || 3000;
+// Set up Router
+app.use("/", indexRoute);
+app.use("/authors", authorRoute);
+
+// Set up DB
 mongoose.connect(process.env.MONGODB_URI, {
     useNewUrlParser: true,
+    useUnifiedTopology: true,
 });
 const db = mongoose.connection;
 db.on("error", (error) => {
@@ -24,7 +33,9 @@ db.on("error", (error) => {
 });
 db.once("open", () => {
     console.log("Connect to Mongoose");
+    app.listen(port, () => {
+        console.log(`Server is running on http://localhost:${port}`);
+    });
 });
-app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
-});
+
+// App listen
